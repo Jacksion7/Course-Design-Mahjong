5325c6c9eed990ccdc0849ef2e272d94cf2ec5c4
wf <2298540110@qq.com>
2024-05-20 10:18:40 +0800

    增加游戏界面按钮 add button on GameScreen Signed-off-by: zhiqiaozhang(Wf) <2298540110@qq.com>
    Signed-off-by: wf <2298540110@qq.com>

--- MahjongGame4/MahjongGame/Mahjong/MahjongGameManager.java
+++ MahjongGame4/MahjongGame/Mahjong/MahjongGameManager.java
@@ -1,88 +1,117 @@
 package Mahjong;
 
-import Item.GameReady;
+import Item.AbstractMahjongGame;
+
+import Players.Player;
+import UI.GameScreen;
 import UI.PlayerListener;
 import ucd.comp2011j.engine.Game;
+
+import java.util.List;
 
 import static UI.Constant.SCREEN_HEIGHT;
 import static UI.Constant.SCREEN_WIDTH;
 
-public class MahjongGameManager implements Game ,GameReady{
+public class MahjongGameManager extends AbstractMahjongGame   {
     private MahjongGame mahjongGame;
     private int playerLives;
     private int playerScore;
+    public static List<MahjongTile> Player_initial_dealTiles;// connect with Game Screen
 
+    public static Player player;
+    private GameScreen gameScreen;
+    public static boolean ifDealTiles= false;// connect with Game Screen
 
     private PlayerListener listener;
-    // zzq add a parameter to the  MahjongGameManager, for connect the UI screens together
+
     public MahjongGameManager(PlayerListener listener) {
         this.listener = listener;
-        //mahjongGame = new MahjongGame();
+        mahjongGame = new MahjongGame();
+        gameScreen = new GameScreen();
     }
 
+    protected void dealTiles() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                MahjongTile tile = deck.drawTile();
+                if (i == 0) {
+                    players[0].drawTile(tile);
+                } else {
+                    computers[i - 1].drawTile(tile);
+                }
+            }
+        }
+        ifDealTiles=true;
+        Player_initial_dealTiles=players[0].hand;
+        player=players[0];
+    }
+
+    public List<MahjongTile> getPlayer_initial_dealTiles() {
+        return Player_initial_dealTiles;
+    }
+
+    public boolean isIfDealTiles() {
+        return ifDealTiles;
+    }
+
+    public static Player getPlayer() {
+        return player;
+    }
 
     public void startGame() {
         System.out.println("游戏开始！");
-        mahjongGame.playGame();
+        startGameScreen();
+        //mahjongGame.playGame();
+        // mahjongGame.dealTiles();
+        dealTiles();
+        //gameScreen.paintTiles(mahjongGame);
+        System.out.println(isGameOver());
+        while (!isGameOver()) {
+//            mahjongGame.testDrawAndDiscard();
+//            mahjongGame.playComputerTurn();
+        }
         System.out.println("游戏结束！");
     }
 
+    public void startGameScreen(){
+        gameScreen.setVisible(true);
+    }
 
     @Override
     public void playGame() {
-        startGame();
-    }
-
-    // use the engine in thr libs, hence zzq need implement the  ucd.comp2011j.engine.Game;
-    // instead of Item.Game;
-    // it may cause some problem, we need a discussion
-    @Override
-    public int getPlayerScore() {
-        return playerScore;
-    }
-
-    @Override
-    public int getLives() {
-        return playerLives;
-    }
-
-    @Override
-    public void updateGame() {
 
     }
 
-    @Override
-    public boolean isPaused() {
-        return false;
-    }
 
-    @Override
-    public void checkForPause() {
+//    @Override
+//    public void playGame() {
+//        startGame();
+//    }
+
+//    public void playGame() {
+//        mahjongGame.dealTiles();
+//        while (!isGameOver()) {
+//            mahjongGame.testDrawAndDiscard();
+//            mahjongGame.playComputerTurn();
+//        }
+//    }
+
+
+
+
+
+
+    protected void testDrawAndDiscard() {
 
     }
 
     @Override
-    public void startNewGame() {
+    protected void playerTurn() {
 
     }
 
     @Override
-    public boolean isLevelFinished() {
-        return false;
-    }
-
-    @Override
-    public boolean isPlayerAlive() {
-        return false;
-    }
-
-    @Override
-    public void resetDestroyedPlayer() {
-
-    }
-
-    @Override
-    public void moveToNextLevel() {
+    protected void updatePlayerHands() {
 
     }
 
@@ -91,24 +120,14 @@ public class MahjongGameManager implements Game ,GameReady{
         return mahjongGame.isGameOver();
     }
 
-    @Override
-    public int getScreenWidth() {
-        return SCREEN_WIDTH;
-    }
-
-    @Override
-    public int getScreenHeight() {
-        return SCREEN_HEIGHT;
-    }
 
 
     public static void main(String[] args) {
-        MahjongGame mahjongGame1 = new MahjongGame();
-        //mahjongGame1.dealTiles();
-        mahjongGame1.playGame();
-
-//        PlayerListener playerListener = new PlayerListener();
-//        MahjongGameManager gameManager = new MahjongGameManager(playerListener);
-//        gameManager.playGame(); // 调用抽象方法
+//        MahjongGame mahjongGame1 = new MahjongGame();
+//         mahjongGame1.dealTiles();
+//
+        PlayerListener playerListener = new PlayerListener();
+        MahjongGameManager gameManager = new MahjongGameManager(playerListener);
+        gameManager.startGame(); // 调用抽象方法
     }
 }
